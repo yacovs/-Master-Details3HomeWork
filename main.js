c3f72926b470f9c7c4028304566f0aec6279490e
yakov salem <yakov.salem@gmail.com>
2022-06-07 00:45:02 +0300

    fix the innerHTML bug & bootstrap

--- main.js
+++ main.js
@@ -30,22 +30,19 @@ function readFromStorage(){
 }
 
 function displaySumColors(colorsList){
-    sumColors.innerText = `The sum colors in the list: ${colorsList.length}`;
+    sumColors.innerText = `Sum colors in the list: ${colorsList.length}`;
 }
 
 function displayColorList(colorsList){
-    colorListTable.InnerHTML = "";
-    let sum = 0;
-    for(let i = 0; i >= colorsList.length; i++){
-        const raw =`
+    colorListTable.innerHTML = "";
+    for(let i = 0; i < colorsList.length; i++){
+        const raw = `
         <tr>
             <td>${colorsList[i]}</td>
             <td style="background-color: ${colorsList[i]}"></td>
         </tr>`;
-       colorListTable.InnerHTML += raw;
-       sum ++;
+        colorListTable.innerHTML += raw;
     }
-    // sumColors.InnerHTML = `The sum colors in the list: ${sum}`;
 }
 
 function validation(){
@@ -61,14 +58,4 @@ function clearForm(){
     textBox.value = "";
     textBox.focus();
 }
-
-
-const temp = document.getElementById("sumColors");
-
-const colorListTable = document.getElementById("colorListTable");
-colorListTable.InnerHTML = `
-<tr>
-    <td>red</td>
-    <td>---</td>
-</tr>`;
 
